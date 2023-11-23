8ee0ec7095d9a5db0a4ee3be48c506cc751e892c
jinchanho <cksgh3924@gmail.com>
2023-11-23 12:12:58 +0900

    환경설정 완료

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React from 'react';
+import "../SCSS/pages/_main.scss";
 
 const Main = () => {
-  return (
-    <div>
-        <h1>mainPage</h1>
-        <span>test</span>
-    </div>
-  )
+    return (
+        <div className="wrap_container">
+            <header>
+                <div>
+                    <span>
+                        100
+                    </span>
+                    <span>
+                        Portfolio
+                    </span>
+                </div>
+                <div>
+                    <span>00Intro</span>
+                    <span>01Work of piece</span>
+                    <span>02Skill abillity</span>
+                    <span>03Project</span>
+                </div>
+            </header>
+            <div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
 }
 
 export default Main
